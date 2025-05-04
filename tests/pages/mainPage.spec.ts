28849f3f68aa49d336d48c4bfbbef9df86788a97
Volkova Maria <MaRi221994@yandex.ru>
2025-05-04 21:17:55 +0700

    add fixtures

--- tests/pages/mainPage.spec.ts
+++ tests/pages/mainPage.spec.ts
@@ -1,27 +1,27 @@
-import { test, expect, Page, Locator } from '@playwright/test';
+import { test, expect } from '../fixtures/mainPage';
 import { MainPage } from '../models/MainPage';
 
-let mainPage: MainPage;
+// let mainPage: MainPage;
 
 test.describe('Тесты главной страницы', () => {
-  test.beforeEach(async ({ page }) => {
-    mainPage = new MainPage(page);
-    await mainPage.openMainPage();
-  });
+  // test.beforeEach(async ({ page }) => {
+  //   mainPage = new MainPage(page);
+  //   await mainPage.openMainPage();
+  // });
 
-  test('Проверка отображения элементов навигации хедера', async () => {
+  test('Проверка отображения элементов навигации хедера', async ({ mainPage }) => {
     await mainPage.checkElementsVisibility();
   });
 
-  test('Проверка названия элементов навигации хедера', async () => {
+  test('Проверка названия элементов навигации хедера', async ({ mainPage }) => {
     await mainPage.checkElementsText();
   });
 
-  test('Проверка атрибутов href элементов навигации хедера', async () => {
+  test('Проверка атрибутов href элементов навигации хедера', async ({ mainPage }) => {
     await mainPage.checkElementsHrefAttribute();
   });
 
-  test('Проверка переключения light мода', async () => {
+  test('Проверка переключения light мода', async ({ mainPage }) => {
     await test.step(`Нажатие на иконку переключения лайт мода`, async () => {
       await mainPage.clickSwitchModelIcon();
     });
@@ -30,7 +30,7 @@ test.describe('Тесты главной страницы', () => {
     });
   });
 
-  test(`Проверка стилей со светлой темой`, async () => {
+  test(`Проверка стилей со светлой темой`, async ({ mainPage }) => {
     await test.step(`Установка светлой темы`, async () => {
       await mainPage.setLightMode();
     });
@@ -39,11 +39,11 @@ test.describe('Тесты главной страницы', () => {
     });
   });
 
-  test(`Проверка стилей с темной темой`, async () => {
+  test(`Проверка стилей с темной темой`, async ({ mainPage }) => {
     await test.step(`Установка темной темы`, async () => {
       await mainPage.setDarkMode();
     });
-    await test.step(`Скриншоная проверка с активной светлой темой`, async () => {
+    await test.step(`Скриншоная проверка с активной темной темой`, async () => {
       await mainPage.checkLayoutWithDarkMode();
     });
   });
